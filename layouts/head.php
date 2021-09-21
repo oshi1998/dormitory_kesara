d648b09b70abb44c4de6cfe92f5ddb1197116ebb
oshi1998 <76892994+oshi1998@users.noreply.github.com>
2021-09-22 01:09:29 +0700

    v1.0.2

--- layouts/head.php
+++ layouts/head.php
@@ -1,5 +1,3 @@
-<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-
 <!-- Bootstrap core CSS -->
 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!--
